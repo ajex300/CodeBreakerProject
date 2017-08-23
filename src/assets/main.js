edca5c73bbeadfa5a0f2cdff279b1c9902bfeae1
ajex300 <ajex300@gmail.com>
2017-08-22 21:08:05 -0600

    Added variable number of codes and guesses
    The number of codes and guesses can be changed and applied by pressing the "Reset Game" button.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,8 +1,11 @@
 var answer = document.getElementById('answer');
 var attempt = document.getElementById('attempt');
-const MAX_ATTEMPTS = 10;
-const MAX_INPUT = 4;
+var max_input = document.getElementById('max_input');
+var max_attempt = document.getElementById('max_attempt');
+var MAX_INPUTS = max_input.value;
+var MAX_ATTEMPTS = max_attempt.value; 
 const RESULTS_DIV = document.getElementById('results').innerHTML;
+const CODE_CLASS_NAME = codeElement = document.getElementById('code').className;
 
 if (answer.value == '' || attempt.value == '')
     setHiddenFields();
@@ -12,10 +15,10 @@ function guess() {
 
     //add functionality to guess function here
     var myinput = input.value;
-    console.log(myinput);
     myinput = validateInput(myinput);
     if (myinput != '')
     {
+//        console.log(myinput);
         attempts = Number(attempt.value);
         if (getResults(myinput, answer.value))
         {
@@ -51,31 +54,57 @@ function showAnswer(winner) {
         codeElement.className = codeElement.className + " failure";
 }
 
+function hideAnswer() {
+    codeElement = document.getElementById('code');
+    codeElement.innerHTML = "<strong>" + "?".repeat(MAX_INPUTS) + "</strong>";
+    codeElement.className = CODE_CLASS_NAME;
+}
+
 function showReplay() {
     document.getElementById('guessing-div').style.display = 'none';
     document.getElementById('replay-div').style.display = 'block';
 }
 
+function hideReplay() {
+    document.getElementById('guessing-div').style.display = 'block';
+    document.getElementById('replay-div').style.display = 'none';
+}
+
 function resetGame() {
     setHiddenFields();
+    hideReplay();
+    hideAnswer();
+    document.getElementById('user-guess').value = '';
+    setMessage("");
     document.getElementById('results').innerHTML = RESULTS_DIV;
 }
 
 function validateInput(input) {
-    console.log(input);
-    if (input.length < MAX_INPUT)
-       return '';
+//    console.log(input);
+//    console.log(input.length);
+//    console.log(MAX_INPUTS);
     if (input[0] == '-' || input[0] == '+')
         input = input.slice(1);
-    if (input.length != MAX_INPUT) {
-        setMessage("Guesses must be exactly " + MAX_INPUT + " digits");
+    if (input.length != MAX_INPUTS) {
+        setMessage("Guesses must be exactly " + MAX_INPUTS + " digits");
         return '';
     }
     return input;
 }
 
 function setHiddenFields() {
-    answer.value = (Math.floor(Math.random() * 10000) + 10000).toString().substring(1);
+    MAX_INPUTS = max_input.value;
+    MAX_ATTEMPTS = max_attempt.value;
+//    console.log(MAX_INPUTS);
+//    console.log(MAX_ATTEMPTS);
+//    answer.value = (Math.floor(Math.random() * 10000) + 10000).toString().substring(1);
+    var seed = 1;
+    for (var i = 0; i < MAX_INPUTS; i++) {
+        seed = seed * 10;
+    }
+//    console.log(seed);
+    answer.value = (Math.floor(Math.random() * seed) + seed).toString().substring(1);
+//    console.log(answer.value);
     attempt.value = '1';
 }
 
